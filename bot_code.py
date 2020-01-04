0ee3b23511faa2b280624a74f3fb76f5f3b5ac26
user <senatorov.nikolay@mail.ru>
2020-01-04 20:54:22 +0300

    'table'

---
+++ bot_code.py
@@ -0,0 +1,20 @@
+import random
+import telebot
+from telebot import types
+
+TOKEN = '878010416:AAHr2zdcA3GihVrt3qwLveTL7wz7vCdvyTM'
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message: types.Message):
+    if message.text == 'Привет':
+        bot.send_message(message.from_user.id, 'Привет')
+
+        keyboard = types.ReplyKeyboardMarkup()
+        key_yes = types.KeyboardButton('Привет песик')
+        keyboard.add(key_yes)
+        key_no = types.KeyboardButton('Иди в будку мудила вонючая')
+        keyboard.add(key_no)
+        bot.send_message(message.from_user.id, text='asnwer', reply_markup=keyboard)
+
+bot.polling()
